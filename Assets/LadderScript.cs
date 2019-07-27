899c632f7a8e399aaf45680472c5bee4f07fffc5
MrMelon <melonmonster.54@gmail.com>
2019-07-27 17:37:07 +0100

    Snakes and Ladders mod

---
+++ Assets/LadderScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour {
+
+	public int start;
+	public int end;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/SquareScript.cs
+++ Assets/SquareScript.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class SquareScript : MonoBehaviour {
 
+	private int num=0;
+	private int col=0;
+	public bool isFinish=false;
+	public GameObject block;
+	public GameObject text;
+	private Material redM;
+	private Material blueM;
+	private Material greenM;
+	private Material yellowM;
+
 	// Use this for initialization
 	void Start () {
-
+		if(isFinish){
+			num=100;
+			col=0;
+		}
 	}
 
 	// Update is called once per frame
@@ -14,11 +27,32 @@ public class SquareScript : MonoBehaviour {
 
 	}
 
+	public void SetMaterials(Material r, Material b, Material g, Material y) {
+		redM=r;
+		blueM=b;
+		greenM=g;
+		yellowM=y;
+	}
+
 	public void SetNumber(int n) {
+		num=n;
+		if(isFinish)return;
+		text.GetComponent<TextMesh>().text=num.ToString();
 		return;
 	}
 
+	public int GetNumber() {
+		return num;
+	}
+
 	public void SetColour(int c) {
+		col=c;
+		if(isFinish)return;
+		block.GetComponent<Renderer>().material=c==0?redM:c==1?blueM:c==2?greenM:yellowM;
 		return;
+	}
+
+	public int GetColour() {
+		return col;
 	}
 }
